2d84ba7bde899637cb4c3807bd2cb3d0ae768d34
Łukasz Mróz <lmroz95@gmail.com>
2022-04-22 20:10:48 +0200

    Add function 'filterBooks'

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@
     containerOf: {
       bookList: '.books-list',
       bookImages: '.book__image',
+      bookFilters: '.filters',
     },
   };
 
@@ -30,10 +31,12 @@
       bookContainer.appendChild(generatedDOM);
     }
   }
-  render();
+  
   
 
   const favoriteBooks = [];
+
+  const filters = [];
 
   function initActions(){
 
@@ -61,8 +64,56 @@
       console.log('favoriteBooks', favoriteBooks);
 
     });
+
+    const booksFilters = document.querySelector(select.containerOf.bookFilters);
+    console.log('booksFilters', booksFilters);
+
+    booksFilters.addEventListener('click', function(callback){
+      const clickElement = callback.target;
+
+      if(clickElement.tagName == 'INPUT' && clickElement.type == 'checkbox' && clickElement.name == 'filter'){
+        console.log('clickElement', clickElement);
+
+        if(clickElement.checked){
+          filters.push(clickElement.value);
+          console.log(filters);
+
+        } else {
+          const indexValue = filters.indexOf(clickElement.value);
+          filters.splice(indexValue, 1);
+
+        }
+      }
+
+      filterBooks();
+
+    }); 
+
+
   }
 
-  initActions();
+  function filterBooks(){
+    for(let book of dataSource.books){
+      let shouldBeHidden;
+      const filterHiddenBooks = document.querySelector('.book__image[data-id="' + book.id + '"]');
 
+      for(const filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+      }
+
+      if(shouldBeHidden){
+        filterHiddenBooks.classList.add('hidden');
+      } else {
+        filterHiddenBooks.classList.remove('hidden');
+      }
+
+    }
+  }
+
+
+  render();
+  initActions();
 } 
